d63d6ac04bc4f5b6fa2dbfa8200bd045ccc26f52
makaiqin <ma.kaiqin@uneed.com>
2019-01-12 19:05:57 +0800

    lsider

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="m-header">
         <div class="icon"></div>
-        <h1 class="text">Emma Music</h1>
+        <h1 class="text">HAHA Music</h1>
     </div>
 </template>
 <script>
