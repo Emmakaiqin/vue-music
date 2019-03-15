46d4e15797b14b837d4684c24c1116580be60b63
Emma <1245095397@qq.com>
2019-03-16 00:35:04 +0800

    歌单数据

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,32 +1,22 @@
 //推荐
 import jsonp from '@assets/js/jsonp'
 import { commonParams, options } from './config'
+// import axios from 'axios'
 export function getRecommend() {
   const url =
     'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
   const data = Object.assign({}, commonParams, {
     platform: 'h5',
     uin: 0,
-    needNewCode: 1,
+    needNewCode: 1
   })
   return jsonp(url, data, options)
 }
-export function getDiscList() {
-  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg'
-  let d = new Date()
-  let date = d.getFullYear() + '_' + (d.getMonth() + 1)
-  const data = Object.assign({}, commonParams, {
-    tpl: 3,
-    page: 'detail',
-    date,
-    topid: 26,
-    type: 'top',
-    song_begin: 0,
-    song_num: 300,
-    loginUin: 0,
-    hostUin: 0,
-    platform: 'yqq',
-    needNewCode: 0,
-  })
-  return jsonp(url, data, options)
-}
+// export function getDiscList() {
+//   return axios.get('./discList.json', res => {
+//     console.log('DiscList.json:', res)
+//     return Promise.resolve(res)
+//   })
+// }
+// img地址
+// https://y.gtimg.cn/music/photo_new/T002R300x300M0000
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,107 +1,133 @@
+
 <template>
-    <div class="recommend">
-        <div class="recommend-content">
-            <div class="slider-wrapper" v-if="recommends.length">
-                <slider>
-                    <div v-for="item in recommends" :key="item.id">
-                        <a :href="item.linkUrl">
-                            <img :src="item.picUrl" />
-                        </a>
-                    </div>
-                </slider>
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper"
+           v-if="recommends.length">
+        <slider>
+          <div v-for="item in recommends"
+               :key="item.id">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" />
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul>
+          <li v-for="item in discList"
+              :key="item.album.id"
+              class='item'>
+            <div class='icon'>
+              <img width='60'
+                   height='60'
+                   :src='item.album.img'
+                   alt=''>
             </div>
-            <div class="recommend-list">
-                <h1 class="list-title">热门歌单推荐</h1>
-                <ul></ul>
+            <div class='text'>
+              <h2 class="name">{{item.album.name}}</h2>
+              <p class="desc">{{item.singerName}}</p>
             </div>
-        </div>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import Slider from "@/base/slider/slider"
 import { getRecommend, getDiscList } from "@/api/recommend"
 import { ERR_OK } from "@/api/config"
+import discList from "./discList.json"
 export default {
-    data() {
-        return {
-            recommends: [],
-            discList: []
+  data () {
+    return {
+      recommends: [],
+      discList: [],
+    }
+  },
+  created () {
+    this._getRecommend()//轮播获取数据
+    this._getDiscList()//歌单数据
+  },
+  components: {
+    Slider
+  },
+  methods: {
+    _getRecommend () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log('recommend数据:', res.data)
+          this.recommends = res.data.slider
         }
+      })
     },
-    created() {
-        this._getRecommend()//轮播获取数据
-        this._getDiscList()//歌单数据
-    },
-    components: {
-        Slider
-    },
-    methods: {
-        _getRecommend() {
-            getRecommend().then((res) => {
-                if (res.code === ERR_OK) {
-                    console.log('recommend数据:', res.data)
-                    this.recommends = res.data.slider
-                }
-            })
-        },
-        _getDiscList() {
-            getDiscList().then((res) => {
-                if (res.code === ERR_OK) {
-                    console.log('discList:', res, res.songlist)
-                    this.discList = res.songlist
-                }
-            })
-        }
-    },
+    _getDiscList () {
+      let list = JSON.parse(JSON.stringify(discList.list))
+      this.discList = list.map(item => {
+        let { album, url, singer } = item
+        //图片地址
+        album.img = 'https://y.gtimg.cn/music/photo_new/T002R300x300M000' + album.mid + '.jpg'
+        let singerName = singer.map(item => {
+          return item.name || ''
+        })
+        singerName = singerName.join(' ')
+        return { album, url, singerName }
+      })
+      console.log(1, this.discList)
+    }
+  },
 }
 </script>
 <style lang="stylus" scoped>
+
 @import '~@assets/css/variable'
 .recommend
-    position fixed
-    width 100%
-    top 88px
-    bottom 0
-    .recommend-content
-        height 100%
-        overflow hidden
-        .slider-wrapper
-            position relative
-            width 100%
-            overflow hidden
-        .recommend-list
-            .list-title
-                height 65px
-                line-height 65px
-                text-align center
-                font-size $font-size-medium
-                color $color-theme
-            .item
-                display flex
-                box-sizing border-box
-                align-items center
-                padding 0 20px 20px 20px
-                .icon
-                    flex 0 0 60px
-                    width 60px
-                    padding-right 20px
-                .text
-                    display flex
-                    flex-direction column
-                    justify-content center
-                    flex 1
-                    line-height 20px
-                    overflow hidden
-                    font-size $font-size-medium
-                    .name
-                        margin-bottom 10px
-                        color $color-text
-                    .desc
-                        color $color-text-d
-        .loading-container
-            position absolute
-            width 100%
-            top 50%
-            transform translateY(-50%)
+  position fixed
+  width 100%
+  top 88px
+  bottom 0
+  .recommend-content
+    height 100%
+    overflow hidden
+    .slider-wrapper
+      position relative
+      width 100%
+      overflow hidden
+    .recommend-list
+      .list-title
+        height 65px
+        line-height 65px
+        text-align center
+        font-size $font-size-medium
+        color $color-theme
+      .item
+        display flex
+        box-sizing border-box
+        align-items center
+        padding 0 20px 20px 20px
+        .icon
+          flex 0 0 60px
+          width 60px
+          padding-right 20px
+        .text
+          display flex
+          flex-direction column
+          justify-content center
+          flex 1
+          line-height 20px
+          overflow hidden
+          font-size $font-size-medium
+          .name
+            margin-bottom 10px
+            color $color-text
+          .desc
+            color $color-text-d
+    .loading-container
+      position absolute
+      width 100%
+      top 50%
+      transform translateY(-50%)
 </style>
 
